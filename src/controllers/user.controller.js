61296f7487ee3b3f30e52c2c3eb5c5a6a95ba525
Tanishka <guptatanishka2008@gmail.com>
2024-05-09 22:00:00 +0530

    add watch history endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,9 @@ import {User} from '../models/user.model.js'
 import uploadonCloudinary from '../utils/cloudinary.js'
 import APIresponse from '../utils/APIResponse.js'
 import jwt from 'jsonwebtoken'
+import fs from 'fs'
+import mongoose from 'mongoose'
+import { pipeline } from 'stream'
 
 const generateRefreshAndAcessToken = async(userId) =>{
  try {
@@ -211,11 +214,268 @@ const refreshAcessToken = asyncHandler(async (req, res) => {
  }
 })
 
+//###
+const changeCurrentPassword = asyncHandler (async (req, res) => {
+
+ const {oldPassword, newPassword, confirmPassword} = req.body;
+ const user = await User.findById(req.user?._id)
+
+ //checking if our old password is correct or not
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+  if(!isPasswordCorrect){
+    throw new APIError(400, "Invalid old password.")
+  }
+
+  user.password = newPassword;
+  user.save({validateBeforeSave: false})
+
+  if(newPassword !== confirmPassword){
+    throw new APIError(401, "Password does not match.")
+  }
+
+  return res
+  .status(200)
+  .json(200, {}, "Password changed Successfully.")
+})
+
+//###
+ const getCurrentUser = asyncHandler( async (req,res)=> {
+  return res
+  .status(200)
+  .json(200, req.user, "current user fetched successfully.")
+ })
+
+
+ //###
+ const updateAccountDetails = asyncHandler( async(req, res) => {
+  const {email, fullName} = req.body
+
+  if(!fullName || !email){
+    throw new APIError(401, "All fields are required.")
+  }
+
+   const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: {
+        fullName,
+        email
+      }
+    },
+    {
+     //Return new information after Update
+      new: true                                                     
+    }
+  ).select("-password")
+
+  return res
+  .status(200)
+  .json(
+    new APIresponse(200, user, "Account details updated Successfully.")
+  )
+})
+
+//###
+const updateAvatar = asyncHandler(async (req, res) => {
+
+ const avatarLocalPath = req.file?.path
+ if(!avatarLocalPath){
+  throw new APIError(401, "cannot find avatar file.")
+ }
+
+ const avatar = await uploadonCloudinary(avatarLocalPath)
+ if(!avatar.url){
+  throw new APIError(401, "Error while uploading avatar.")
+ }
+
+ const user = await User.findByIdAndUpdate(
+  req.user._id,
+  {
+    $set: {
+      avatar : avatar.url
+    }
+  },
+  {new : true}
+).select("-password")
+
+ const oldImageToBeDeleted = fs.unlinkSync(avatar.url);
+
+return res
+.status(200)
+.json(
+  new APIresponse(200, user, "Avatar uploaded successfully.")
+)
+})
+
+//###
+const updateCoverImage = asyncHandler(async (req, res) => {
+
+  const coverImageLocalPath = req.file?.path
+  if(!coverImageLocalPath){
+   throw new APIError(401, "cannot find cover Image file.")
+  }
+ 
+  const coverImage = await uploadonCloudinary(avatarLocalPath)
+  if(!coverImage.url){
+   throw new APIError(401, "Error while uploading cover Image.")
+  }
+ 
+  const user = await User.findByIdAndUpdate(
+   req.user._id,
+   {
+     $set: {
+       coverImage : coverImage.url
+     }
+   },
+   {new : true}
+ ).select("-password")
+ 
+ return res
+ .status(200)
+ .json(
+   new APIresponse(200, user, "Cover Image uploaded successfully.")
+ )
+ })
+
+ //###
+ const getUserChannelProfiile = asyncHandler(async (req, res) => {
+  const {userName} = req.params;
+
+  if(!userName?.trim()){
+    throw new APIError(400, "cannot find username.")
+  }
+
+   const channel = await User.aggregate([
+    //first pipeline
+    {
+      $match: {
+        userName : userName?.toLowerCase()
+      }
+    },
+    {
+      $lookup: {
+        from : "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers"
+      }
+    },
+    {
+      $lookup: {
+        from : "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribed"
+      }
+    },
+    {
+      $addFields: {
+
+        subscribersCount: {
+          $size : "$subscribers"
+        },
+        subscribedToCount: {
+          $size : "$subscribed"
+        },
+        isSubscribed : {
+          $cond : {
+            $if : {$in: [req.user._id,  "$subscribers.subscriber"]},
+            then : true,
+            else: false
+          }
+        }
+      }
+    },
+    //second pipeline
+    {
+      $project: {
+        fullName: 1,
+        userName: 1,
+        email: 1,
+        avatar: 1,
+        coverImage: 1,
+        subscribersCount: 1,
+        subscribedToCount: 1,
+        isSubscribed: 1,
+      }
+    }
+  ])
+  console.log(channel)
+
+  if(!channel?.length){
+   throw new APIError(400, "channel does not exist.")
+  }
+
+  res
+  .status(200)
+  .json(
+    new APIresponse(200, channel[0], "channel fetched successfully.")
+  )
+ })
+
+ //###
+ const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user?._id)
+      }
+    },
+    {
+   $lookup: {
+      from: "videos",
+      localField: "watchHistory",
+       foreignField: "_id",
+       as: "watchHistory",
+
+       pipeline: [
+          {
+         $lookup: {
+         from: "users",
+         localField: "owner",
+         foreignField: "_id",
+         as: "owner",
+
+         pipeline: [{
+         $project: {
+          fullName: 1,
+          userName: 1,
+          avatar: 1
+       }
+     }]
+     }
+   },
+  {
+    $addFields:{
+    videoOwner:{
+     $first: "$owner"
+       }
+      }
+     }]
+    }
+  }
+])
+
+res
+.status(200)
+.json(
+ new APIresponse(200, user[0].watchHistory, "watch history fetched successfully.")
+)
+
+ })
 
 
 export {
   registerUser,
   loginUser,
   logoutUser,
-  refreshAcessToken
+  refreshAcessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updateAvatar,
+  updateCoverImage,
+  getUserChannelProfiile,
+  getWatchHistory
 }
